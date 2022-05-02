427a79f712e032e86eae24bcce7656f525bb3ae4
dsvivass <dsvivass@unal.edu.co>
2022-05-01 23:37:45 -0500

    Punto 3 desarrollado, todos los test pasan satisfactoriamente

--- src/app/planta/planta-listar/planta-listar.component.spec.ts
+++ src/app/planta/planta-listar/planta-listar.component.spec.ts
@@ -2,15 +2,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { PlantaListarComponent } from './planta-listar.component';
+import { Planta } from '../planta';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PlantaListarComponent', () => {
   let component: PlantaListarComponent;
   let fixture: ComponentFixture<PlantaListarComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ PlantaListarComponent ]
     })
     .compileComponents();
@@ -19,10 +24,52 @@ describe('PlantaListarComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantaListarComponent);
     component = fixture.componentInstance;
+
+    component.plantas = [
+      new Planta(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      ),
+      new Planta(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      ),
+      new Planta(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+      ),
+    ];
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Tabla creada satisfactoriamente', () => {
+    const tableItem = fixture.debugElement.query(By.css('table'));
+    expect(tableItem).toBeTruthy();
+  });  
+
+  it('Elementos añadidos a la tabla correctamente', () => {
+    const rows = debug.nativeElement.querySelectorAll('tr');
+    expect(rows.length).toBe(4);
+  });  
+  
 });

cd6454caaa7386d8df83622b91eccb209ec058a3
dsvivass <dsvivass@unal.edu.co>
2022-05-01 22:39:21 -0500

    Punto 2 desarrollado

--- src/app/planta/planta-listar/planta-listar.component.ts
+++ src/app/planta/planta-listar/planta-listar.component.ts
@@ -10,13 +10,34 @@ import {PlantaService} from '../planta.service';
 export class PlantaListarComponent implements OnInit {
 
   plantas: Array<Planta> = [];
+  tipos: Array<{ [key: string]: any }> = [];
 
   constructor(private plantaService: PlantaService) { }
 
   traerPlantas(): void {
     this.plantaService.traerPlantas().subscribe((plantas) => {
       this.plantas = plantas;
+      this.tipoContador();
     });
+  }
+
+  tipoContador(): void {
+    let contador: { [key: string]: number } = {};
+
+    this.plantas.forEach(planta => {
+      if (contador.hasOwnProperty(planta.tipo)) {
+        contador[planta.tipo]++;
+      } else {
+        contador[planta.tipo] = 1;
+      }
+    });
+
+    let tipos: Array<{ [key: string]: any }> = [];
+    Object.keys(contador).forEach(key => {
+      tipos.push({ 'cantidad': contador[key], 'tipo': key });
+    });
+
+    this.tipos = tipos;
   }
 
   ngOnInit() {
